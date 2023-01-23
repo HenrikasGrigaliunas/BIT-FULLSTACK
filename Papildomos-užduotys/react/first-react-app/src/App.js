496c58adfaf3a130c849f04d8318d452cfa3a520
HenrikasGrigaliunas <grigaliunas.h@gmail.com>
2023-01-23 12:28:37 +0200

    update

--- Papildomos-užduotys/react/first-react-app/src/App.js
+++ Papildomos-užduotys/react/first-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sveiki, tai mano pirmoji React aplikacija
+          Sveiki, tai mano pirmoji React aplikacija.
         </p>
        
       </header>
